57327daa94d852da1d313aa61c9e7e4239afaaf1
N-LEE-94 <nathanlee42@btinternet.com>
2022-07-04 15:41:59 +0100

    form changes

--- src/Form/form.js
+++ src/Form/form.js
@@ -1,5 +1,6 @@
 function Form () {
 return (<form name="contact" method="POST" data-netlify="true">
+ <input type="hidden" name="form-name" value="contact" />
   <p>
     <label>Your Name: <input type="text" name="name" /></label>
   </p>
