21ab1257e3b171f4ce6219d28f2436c031696c51
N-LEE-94 <nathanlee42@btinternet.com>
2022-07-04 15:31:13 +0100

    added form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Form from './Form/form.js'
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
         <p>
           Hello World, from Steven and Nathan!
         </p>
+        <Form />
       </header>
     </div>
   );
